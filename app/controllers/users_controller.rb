c02bc61f8e55ac54a55a2d23955c9a0a60e67039
Mayra Cabrera <mcabrera1087@gmail.com>
2016-04-03 16:38:07 -0500

    Add role attribute to user
    This will help to identify regular users from admin users. Views & model modifications are includd. Close #8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email)
+      params.require(:user).permit(:first_name, :last_name, :email, :role)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ActiveRecord::Base
+  ADMIN = "admin"
+  REGULAR = "regular"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, :email, :role, presence: true
   has_many :orders
 
   def complete_name
     "#{first_name} #{last_name}"
+  end
+
+  def admin?
+    role == "ADMIN"
+  end
+
+  def regular?
+    role == "REGULAR"
   end
 end

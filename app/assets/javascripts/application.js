146281d2f977794c84d4cb5629a54fae331f71cc
Mayra Cabrera <mcabrera1087@gmail.com>
2016-04-24 16:23:42 -0500

    Add styles for frontend section. Includes complete functionality for navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,29 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require frontend/bootstrap.min.js
+//= require frontend/flexslider.min.js
+//= require frontend/masonry.min.js
+//= require frontend/smooth-scroll.min.js
+//= require frontend/parallax.js
+//= require frontend/scripts.js
+//= require frontend/slick.min.js
 //= require turbolinks
-//= require_tree .
+//
+$('.multiple-items').slick({
+      infinite: true,
+      autoplay: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: true,
+      dots:false,
+      responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots:true,
+      }
+    }
+  ]
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
  layout 'application'
   def index
+    @brands = Brand.includes(:models)
+    @collections = Collection.all.each_slice(Collection.count / 2)
   end
 end

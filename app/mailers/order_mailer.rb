5c92139223b6c9c9cb93ad1e7a35531e60cca73b
Mayra Cabrera <mcabrera1087@gmail.com>
2016-05-15 13:51:44 -0500

    Update production environment and set absolut path for order mailer images

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,11 @@ class OrderMailer < ApplicationMailer
   def send_confirmation order, user
     @order = order
     @line_items = order.line_items
+    if Rails.env.development?
+      @server_name = ""
+    else
+      @server_name = "http://ec2-54-213-190-137.us-west-2.compute.amazonaws.com/"
+    end
     mail(to: user.email, subject: "Cotización ##{order.id}")
   end
 end

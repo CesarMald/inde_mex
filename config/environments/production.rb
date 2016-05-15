97f78d10b02b8d613cbb52e04617af8ea2278f04
Mayra Cabrera <mcabrera1087@gmail.com>
2016-05-15 17:49:16 -0500

    Remove production email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings  = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'smtp.gmail.com',
-    user_name: 'mayra@icalialabs.com',
-    password: 'SvP3sukX20wjSi',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.perform_deliveries = false
+#  config.action_mailer.smtp_settings  = {
+#    address: "smtp.gmail.com",
+#    port: 587,
+#    domain: 'smtp.gmail.com',
+#    user_name: 'mayra@icalialabs.com',
+#    password: 'SvP3sukX20wjSi',
+#    authentication: 'plain',
+#    enable_starttls_auto: true
+#  }
   config.action_mailer.raise_delivery_errors = false
 end

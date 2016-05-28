18babb88cdbbc86f79ad3da51f166493e5275877
Mayra Cabrera <mcabrera1087@gmail.com>
2016-05-28 14:58:13 -0500

    Add public/uploads to linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :passenger_restart_with_touch, true
 set :deploy_to, "/home/ubuntu/idenmex"
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :use_sudo, false
 
 
